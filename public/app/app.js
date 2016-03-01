f0f23bfc061f16c7963fe8885bc2fae8dd331f6a
Sandeep <sandy.wadhwa@gmail.com>
2016-03-01 09:54:10 +0530

    Adding More Partials for SPA...

--- public/app/app.js
+++ public/app/app.js
@@ -1,10 +1,19 @@
 angular.module('app', ['ngRoute', 'ngResource']);
 
 angular.module('app').config(function($routeProvider, $locationProvider){
-    $routeProvider.
-        when('/', {templateUrl:'/partials/main', controller:'mainCtrl'});
+    $routeProvider.when('/', {templateUrl:'/partials/main', controller:'mainCtrl'})
+        .when('/login', {templateUrl:'/partials/login', controller:'loginCtrl'})
+        .when('/signup', {templateUrl:'/partials/signup', controller:'signupCtrl'});
 });
 
 angular.module('app').controller('mainCtrl', function($scope){
     $scope.bzVar = "BZ Angular Scope Variable";
+});
+
+angular.module('app').controller('loginCtrl', function($scope){
+    
+});
+
+angular.module('app').controller('signupCtrl', function($scope){
+   
 });
